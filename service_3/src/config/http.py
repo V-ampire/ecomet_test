4d9c995931d2d68881232a83f65b5fc1fbf91ffe
v-ampire <webjob010@gmail.com>
2025-10-28 14:20:52 +0300

    remove unused

--- service_3/src/config/http.py
+++ service_3/src/config/http.py
@@ -4,19 +4,12 @@ import aiohttp
 from aiohttp import ClientError
 from tenacity import stop_after_attempt, wait_exponential, retry, retry_if_exception_type
 
-retry_params = dict(
+retry_helper = retry(
     retry=retry_if_exception_type(ClientError),
     stop=stop_after_attempt(3),
     wait=wait_exponential(multiplier=1, min=1, max=3),
+    reraise=True
 )
-
-
-retry_helper = retry(
-        retry=retry_if_exception_type(ClientError),
-        stop=stop_after_attempt(3),
-        wait=wait_exponential(multiplier=1, min=1, max=3),
-        reraise=True
-    )
 
 
 def http_session_factory(
